ff56fa292ad52322f6cd079e984a107bbbd89b98
Khairul Imam <ki65559@gmail.com>
2017-02-17 16:08:12 +0800

    filter explicitly

---
+++ src/main/java/com/klinik/dev/enums/FILTERABLE.java
@@ -0,0 +1,10 @@
+package com.klinik.dev.enums;
+
+/**
+ * Created by khairulimam on 17/02/17.
+ */
+public enum FILTERABLE {
+    FILTER_BY_NAME_OR_RM,
+    FILTER_BY_TINDAKAN,
+    FILTER_BY_TANGGAL
+}
